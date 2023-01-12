c57a7061f569c19a4d4dc7780f08b75026e6c69d
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2023-01-12 19:22:37 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,47 +1,20 @@
 #!/usr/bin/python3
-'''module contains solution to min-operations problem'''
-import math
-
-
-def highestPrimeFactor(n):
-    '''finds highest prime factor of n'''
-    max_prime = 0
-
-    while n % 2 == 0:
-        max_prime = 2
-        n = n / 2
-
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            max_prime = i
-            n = n / i
-
-    if n > 2:
-        max_prime = n
-
-    return int(max_prime)
-
-
-def isPrime(n):
-    ''' checks whether number is prime or not '''
-    flag = False
-
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                flag = True
-                break
-    return not flag
+"""
+Contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
 
 def minOperations(n):
-    ''' finds number of minimum of operations to get n number of H '''
-    if n <= 1:
-        return 0
-
-    if n == 2 or isPrime(n):
-        return n
-
-    hpf = highestPrimeFactor(n)
-    resultOfDivision = n / hpf
-    return int(hpf + resultOfDivision)
+    """
+    Returns the fewest number of operations needed to result in exactly
+    n H characters in the file.
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
