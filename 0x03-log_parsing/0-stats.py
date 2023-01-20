c38483c98f335dbe4b671b1db2fb5844f6c78153
Faithful Onoriobakpo <faithful.onoriobakpo@gmail.com>
2023-01-20 05:28:33 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,34 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
+'''a script that reads stdin line by line and computes metrics'''
 
-if __name__ == '__main__':
 
-    import sys
+import sys
 
-    def print_results(statusCodes, fileSize):
-        """ Print statistics """
-        print("File size: {:d}".format(fileSize))
-        for statusCode, times in sorted(statusCodes.items()):
-            if times:
-                print("{:s}: {:d}".format(statusCode, times))
+# Initialize variables to store metrics
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-    statusCodes = {"200": 0,
-                   "301": 0,
-                   "400": 0,
-                   "401": 0,
-                   "403": 0,
-                   "404": 0,
-                   "405": 0,
-                   "500": 0
-                   }
-    fileSize = 0
-    n_lines = 0
 
-    try:
-        """ Read stdin line by line """
-        for line in sys.stdin:
-            if n_lines != 0 and n_lines % 10 == 0:
-                """ After every 10 lines, print from the beginning """
-                print_results(statusCodes, fileSize)
-            n_lines += 1
-            data = line.split()
-            try:
-                """ Compute metrics """
-                statusCode = data[-2]
-                if statusCode in statusCodes:
-                    statusCodes[statusCode] += 1
-                fileSize += int(data[-1])
-            except:
-                pass
-        print_results(statusCodes, fileSize)
-    except KeyboardInterrupt:
-        """ Keyboard interruption, print from the beginning """
-        print_results(statusCodes, fileSize)
-        raise
+# Read input line by line
+for line in sys.stdin:
+    # Check if line matches the specified format
+    parts = line.split()
+    if len(parts) != 6:
+        continue
+    ip, date, request, status, code, size = parts
+    status_code = int(code)
+    # Check if status code is in the list of possible codes
+    if status_code not in status_codes:
+        continue
+    # Update metrics
+    total_size += int(size)
+    status_codes[status_code] += 1
+    line_count += 1
+    # Print metrics every 10 lines
+    if line_count % 10 == 0:
+        print("Total file size: ", total_size)
+        for code in sorted(status_codes):
+            print(f"{code}: {status_codes[code]}")
+ 
